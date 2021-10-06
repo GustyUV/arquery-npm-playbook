f8c31779fb65ace67a3e10a72af65f61be12a7da
Arquery <sgu_sigu@msn.com>
2021-10-06 15:21:16 -0600

    Beta version

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = function(){
-	console.log("Hello World");
+module.exports = function(lang){
+	if(lang === "es"){
+		console.log("Hola Mundo");	
+	}
+	else {
+		console.log("Hello World");	
+	}
 }
